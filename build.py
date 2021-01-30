9e1dea17ea5c18e8720636b44a41267dfe25f121
Tachibana Kanade <h0m54r@twitter.com>
2021-01-30 19:36:58 +0000

    basic integrity checks for objects and personae

--- build.py
+++ build.py
@@ -1,11 +1,13 @@
 import json
 import os
-from typing import Callable, Dict, List, TextIO, Union
+import logging
+from typing import Callable, Dict, List, Set, TextIO, Union
 
 from bs4 import BeautifulSoup
 
 
 HERE = os.path.dirname(__file__)
+STRICT = False
 
 
 def parse_object(obj_file: TextIO) -> Dict[str, Union[str, bool]]:
@@ -38,14 +40,37 @@ def parse_persona(person_file: TextIO) -> Dict[str, List[str]]:
     return persona
 
 
+def is_valid(thing: Dict[str, List[str]], fields: Set[str]) -> bool:
+    key_set = set(thing.keys())
+    if key_set != fields:
+        messages = [f'{str(thing)[:50]}...:']
+        if fields - key_set != set():
+            messages.append(f' - is missing keys {fields - key_set}')
+        if key_set - fields != set():
+            messages.append(f' - has unrecognised keys {key_set - fields}')
+        message = '\n'.join(messages)
+
+        if STRICT:
+            raise ValueError(message)
+        else:
+            logging.warning(message)
+            return False
+
+    return True
+
+
 def build_things(folder_name: str, parser: Callable) -> str:
     objects = []
+    fields_filename = os.path.join(folder_name, 'fields.list')
+    fields = set([field.strip() for field in open(fields_filename, 'r').readlines() if field.strip()])
 
     for object_file_name in os.listdir(os.path.join(HERE, folder_name)):
         if object_file_name.startswith('.') or not object_file_name.endswith('.txt'):
             continue
         with open(os.path.join(HERE, folder_name, object_file_name)) as object_file:
-            objects.append(parser(object_file))
+            thing = parser(object_file)
+            if is_valid(thing, fields):
+                objects.append(thing)
 
     return json.dumps(objects, indent=2)
 
