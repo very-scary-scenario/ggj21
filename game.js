6de9a7ee4cbb359fdaec8c6d406c9b078103e028
colons <colons@colons.co>
2021-01-30 13:48:12 +0000

    Implement a very mvp speech system.

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
-console.log('henlo');
+const textBox = document.getElementById("text-box");
+const kioskWindow = document.getElementById("kiosk-window");
+
+function showText(string, callback) {
+  textBox.innerText = string;
+  function advanceText() {
+    document.removeEventListener("click", advanceText);
+    textBox.innerText = "";
+    callback();
+  }
+  document.addEventListener("click", advanceText);
+}
+
+function showTexts(strings, callback) {
+  function nextText() {
+    if (strings.length > 0) {
+      showText(strings.shift(), nextText)
+    } else {
+      callback()
+    }
+  }
+
+  nextText()
+}
+
+showTexts(["henlo", "it me", "a customer"], function() {alert("we're done here")})
