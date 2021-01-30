98ca08806ab530b86fc7920d803a3a7c5f16a5e1
colons <colons@colons.co>
2021-01-30 17:34:43 +0000

    Let people ask about properties (but don't really respond meaningfully).

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const textBox = document.getElementById("text-box");
-const kioskWindow = document.getElementById("kiosk-window");
-const objects = JSON.parse(document.getElementById("objects").textContent);
+const kioskWindow = document.getElementById("kiosk-window"); const objects = JSON.parse(document.getElementById("objects").textContent);
 const personas = JSON.parse(document.getElementById("personas").textContent);
+const properties = document.getElementById("properties");
+const propertyList = document.getElementById("property-list");
 
 function pick(list) {
   return list[Math.floor(Math.random() * list.length)]
@@ -35,6 +36,33 @@ function format(string, repl) {
   return string.replace(/<.*?>/g, repl)
 }
 
+function askAboutPropertyOf(object) {
+  function askAboutProperty(event) {
+    event.stopPropagation()
+    event.preventDefault()
+    var property = event.currentTarget.getAttribute("data-property")
+    console.log(object)
+    console.log(property)
+    console.log(object[property])
+  }
+  return askAboutProperty
+}
+
+function letPlayerAskAboutProperty(object) {
+  properties.classList.add("shown")
+  for (var property in object) {
+    if (["Object", "FlavourText"].indexOf(property) !== -1) { continue }
+    var propertyLink = document.createElement("a")
+    propertyLink.innerText = property
+    propertyLink.setAttribute("href", "#")
+    propertyLink.setAttribute("data-property", property)
+    propertyLink.addEventListener("click", askAboutPropertyOf(object))
+    var propertyListItem = document.createElement("li")
+    propertyListItem.appendChild(propertyLink)
+    propertyList.appendChild(propertyListItem)
+  }
+}
+
 function havePersonaVisit() {
   const persona = pick(personas)
   const object = pick(objects)
@@ -42,7 +70,7 @@ function havePersonaVisit() {
     format(pick(persona.Intro1), object.Object),
     format(pick(persona.Intro2), object.Object),
     format(pick(persona.Intro3), object.Object),
-  ], function() { alert('not implemented :<') })
+  ], function() { letPlayerAskAboutProperty(object) })
 }
 
 havePersonaVisit()
