757a2b36a8533a41c6365a7d3585add62810ca61
colons <colons@colons.co>
2021-02-06 19:29:38 +0000

    Implement Zelda-style redtext.

--- game.js
+++ game.js
@@ -12,35 +12,46 @@ const shelves = document.getElementById("shelves")
 const backRoomEntrance = document.getElementById("back-room-entrance")
 const backRoomExit = document.getElementById("back-room-exit")
 
+const filterExp = /(?<leadingContent>[^<>]*)(?<tag><(?<name>[^>|]+)\|?(?<filter>[^>|]+)?>)?/g
+
+// these text things are globals so that you can, at any time, hand in a new
+// set of texts to override everything and not heck everything up
+
+let textQueue = []
+let textOverCallback = () => {}
+
 function pick(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
 
-function showText(string, callback) {
-  textBox.innerText = string
-  function advanceText(event) {
-    if (backRoom.classList.contains('shown') || properties.classList.contains('shown')) {
-      return  // a modal is open and we should not advance text in the background
-    }
-
-    document.removeEventListener("click", advanceText)
-    textBox.classList.remove("showing-text")
-    textBox.innerText = ""
-    callback()
+function advanceText(event) {
+  if (backRoom.classList.contains('shown') || properties.classList.contains('shown')) {
+    return  // a modal is open and we should not advance text in the background
   }
-  textBox.classList.add("showing-text")
-  document.addEventListener("click", advanceText)
+
+  textBox.classList.remove("showing-text")
+  textBox.innerHTML = ""
+  nextText()
 }
 
-function showTexts(strings, callback) {
-  function nextText() {
-    if (strings.length > 0) {
-      showText(strings.shift(), nextText)
-    } else {
-      callback()
-    }
-  }
+function showText(text, callback) {
+  textBox.innerHTML = ""
+  textBox.appendChild(text)
+  textBox.classList.add("showing-text")
+}
 
+function nextText() {
+  if (textQueue.length > 0) {
+    showText(textQueue.shift(), nextText)
+  } else {
+    textOverCallback()
+    textOverCallback = () => {}
+  }
+}
+
+function showTexts(texts, callback) {
+  textQueue = texts  
+  textOverCallback = callback
   nextText()
 }
 
@@ -51,18 +62,31 @@ const filters = {
 }
 
 function format(string, repl) {
-  const filterExp = /<([^>|]*)\|?([^>|]*)?>/g
-  return string.replace(filterExp, (match, name, filter) => {
-    let replacement = repl(name)
-    if (filter) {
-      if (filters[filter]) {
-        replacement = filters[filter](replacement)
-      } else {
-        throw "no filter exists for " + filter
-      }
+  // take a string and format <blocks> within it, and return a paragraph
+  // element in which each of those blocks is wrapped in a <span> element
+
+  const p = document.createElement('p')
+
+  for (const match of string.matchAll(filterExp)) {
+    if (match.groups.leadingContent) {
+      p.appendChild(document.createTextNode(match.groups.leadingContent))
     }
-    return replacement
-  })
+    if (match.groups.name) {
+      let replacement = repl(name)
+      if (match.groups.filter) {
+        if (filters[match.groups.filter]) {
+          replacement = filters[match.groups.filter](replacement)
+        } else {
+          throw "no filter exists for " + match.groups.filter
+        }
+      }
+      const span = document.createElement('span')
+      span.innerText = replacement
+      p.appendChild(span)
+    }
+  }
+
+  return p
 }
 
 function askPersonaAbout(persona, object, property, callback) {
@@ -83,7 +107,7 @@ function askPersonaAbout(persona, object, property, callback) {
       return object[lookup]
     }
   }
-  showText(format(pick(persona[lookup]), formatReplacementInResponse), callback)
+  showTexts([format(pick(persona[lookup]), formatReplacementInResponse)], callback)
 }
 
 function askAboutPropertyOf(persona, object, callback) {
@@ -186,5 +210,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     havePersonaVisit()
     backRoomEntrance.addEventListener('click', showBackRoom)
     backRoomExit.addEventListener('click', hideBackRoom)
+    document.addEventListener("click", advanceText)
   }, 5)  // wait a second so that the introductory swooce can happen
 })
